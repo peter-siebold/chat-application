f26c9eb1b72cbff910f043dd125668d2cc4d4577
Peter Siebold <peter@ilove.codes>
2023-05-07 20:38:17 +0200

    Added SignUp Form UI

---
+++ app/components/inputs/Input.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import clsx from "clsx";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
+
+interface InputProps {
+  label: String;
+  id: string;
+  type?: string;
+  required?: string;
+  register: UseFormRegister<FieldValues>;
+  errors: FieldErrors;
+  disabled?: boolean;
+}
+
+const Input: React.FC<InputProps> = ({
+  label,
+  id,
+  type,
+  required,
+  register,
+  errors,
+  disabled,
+}) => {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className="block text-sm font-medium öeading-6 text-gray-900"
+      >
+        {label}
+      </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `form-input 
+            block 
+            w-full 
+            rounded-md 
+            border-0 
+            py-1.5 
+            text-gray-900 
+            shadow-sm 
+            ring-1 
+            ring-inset 
+            ring-gray-300 
+            focus:ring 
+            focus:ring-inset 
+            focus:ring-sky-600
+            sm:leading-6`,
+            errors[id] && "ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
