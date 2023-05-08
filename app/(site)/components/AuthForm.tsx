0581c0462ed5471751de62432e2d22efb8b7b6b5
Peter Siebold <peter@ilove.codes>
2023-05-08 20:44:46 +0200

    Added Error Notifications

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,7 @@ import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+import { toast } from "react-hot-toast";
 type Variant = "LOGIN" | "REGISTER";
 
 const AuthForm = () => {
@@ -37,7 +38,10 @@ const AuthForm = () => {
     setIsLoading(true);
     if (variant === "REGISTER") {
       console.log("Axios register");
-      axios.post("/api/register", data);
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong"))
+        .finally(() => setIsLoading(false));
     }
     if (variant === "LOGIN") {
       console.log("NextAuth SignIn");
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,25 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import ToasterContext from "./context/ToasterContext";
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Chat Application',
-  description: 'ilc chat application',
-}
+  title: "Chat Application",
+  description: "ilc chat application",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ToasterContext />
+        {children}
+      </body>
     </html>
-  )
+  );
 }
